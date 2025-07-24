e6c5f2edbe025ebbab21bea04606c32d2ea8ac80
Navya cs <navyacshaji12@gmail.com>
2025-07-24 15:44:16 +0530

    Time: 18 ms (62.87%), Space: 63.8 MB (99.93%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,9 +2,14 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-   
-    const s = new Set(nums); return s.size !== nums.length
 
-
+ var containsDuplicate = function(nums) {
+ let a=new Set(nums)
+ let b=Array.from(a)
+ if(b.length==nums.length){
+    return false
+ }
+return true
 };
+
+
