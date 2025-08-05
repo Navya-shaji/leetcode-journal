06f769a94b5c69180ec2e50444e2bbb5afc82411
Navya cs <navyacshaji12@gmail.com>
2025-08-05 21:41:15 +0530

    Time: 1 ms (95.28%), Space: 55.6 MB (86.79%) - LeetHub

---
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} fruits
+ * @param {number[]} baskets
+ * @return {number}
+ */
+var numOfUnplacedFruits = function (fruits, baskets) {
+    let count = 0
+    for (let i = 0; i < fruits.length; i++) {
+        for (let j = 0; j < baskets.length; j++) {
+            if (fruits[i] <= baskets[j]) {
+                baskets[j] = 0
+                count++
+                break
+            }
+        }
+    }
+    return fruits.length-count
+};
