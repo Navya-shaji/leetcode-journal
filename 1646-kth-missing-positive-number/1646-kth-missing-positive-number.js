f8312136c5e411e9f747b156e2f500831b3d1217
Navya cs <navyacshaji12@gmail.com>
2025-08-26 21:47:42 +0530

    Time: 10 ms (10.07%), Space: 55.6 MB (14.9%) - LeetHub

---
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findKthPositive = function(arr, k) {
+    let result=[]
+    for(let i=1;i<=arr.length+k;i++){
+     if(!arr.includes(i)){
+        result.push(i)
+     }
+    }
+return result[k-1]
+};
