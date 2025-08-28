95d15024c3b524fd95e0273d237b4b3d6b977e51
Navya cs <navyacshaji12@gmail.com>
2025-08-28 14:56:24 +0530

    Time: 0 ms (100%), Space: 54.2 MB (19.32%) - LeetHub

---
+++ 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function (nums, index) {
+    let result = []
+
+    for (let i = 0; i < nums.length; i++) {
+        result.splice(index[i], 0, nums[i])
+    }
+    return result
+};
