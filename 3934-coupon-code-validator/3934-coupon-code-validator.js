1ce8af4c0431dee9ed4bb0f3f02252dadbac6a58
Navya cs <navyacshaji12@gmail.com>
2025-09-15 09:31:57 +0530

    Time: 3 ms (93.75%), Space: 60.1 MB (96.53%) - LeetHub

--- 3934-coupon-code-validator/3934-coupon-code-validator.js
+++ 3934-coupon-code-validator/3934-coupon-code-validator.js
@@ -3,15 +3,33 @@
  * @param {string[]} businessLine
  * @param {boolean[]} isActive
  * @return {string[]}
- */
-var validateCoupons = function (code, businessLine, isActive) {
-    let keys = [];
-    const categories = ["electronics", "grocery", "pharmacy", "restaurant"];
-    for (let i = 0; i < code.length; i++) {
-        if (/^[a-zA-Z0-9_]+$/.test(code[i]) && categories.includes(businessLine[i]) && isActive[i]) {
-            keys.push(i);
-        }
+*/
+var validateCoupons = (code, businessLine, isActive) => {
+  const validElectronics = [];
+  const validGrocery = [];
+  const validPharmace = [];
+  const validRestaurant = [];
+  const categories = new Set(['electronics', 'grocery', 'pharmacy', 'restaurant']);
+  for (let i = 0; i < code.length; i++) {
+    if (isActive[i] && categories.has(businessLine[i]) && /^[a-z_0-9]+$/i.test(code[i])) {
+      switch (businessLine[i]) {
+        case 'electronics':
+          validElectronics.push(code[i]);
+          break;
+        case 'grocery':
+          validGrocery.push(code[i]);
+          break;
+        case 'pharmacy':
+          validPharmace.push(code[i]);
+          break;
+        default:
+          validRestaurant.push(code[i]);
+      }
     }
-
-    return keys.sort((i, j) => businessLine[i].localeCompare(businessLine[j]) || (code[i] > code[j] ? 1 : (code[i] === code[j] ? 0 : -1))).map(i => code[i]);
+  }
+  validElectronics.sort();
+  validGrocery.sort();
+  validPharmace.sort();
+  validRestaurant.sort();
+  return validElectronics.concat(validGrocery, validPharmace, validRestaurant);
 };
