78cd288980d8c98790247ab23cd9ebbef3c8700f
Navya cs <navyacshaji12@gmail.com>
2025-10-27 20:54:39 +0530

    Time: 0 ms (100%), Space: 56.2 MB (18.03%) - LeetHub

---
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function (mat) {
+    let sum = 0;
+    for (let i = 0; i < mat.length; i++) {
+        for (let j = 0; j < mat.length; j++) {
+            if (i == j || j == mat.length - i - 1) {
+                sum += mat[i][j];
+            }
+        }
+    }
+    return sum;
+};
