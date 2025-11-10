7a2fe4abeca6daf6b237bd4052b41ada4dba1293
Navya cs <navyacshaji12@gmail.com>
2025-11-10 20:07:50 +0530

    Time: 0 ms (100%), Space: 57.8 MB (39.5%) - LeetHub

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} multiplier
+ * @return {number[]}
+ */
+var getFinalState = function (nums, k, multiplier) {
+    for (let i = 0; i < k; i++) {
+        let Min = Math.min(...nums)
+        let index = nums.indexOf(Min)
+        nums[index] = Min * multiplier
+    }
+    return nums
+};
