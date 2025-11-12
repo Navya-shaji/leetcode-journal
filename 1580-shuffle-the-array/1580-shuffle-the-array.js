283b5a1b3d29646bd32c83c2a43ac33d8a49aecb
Navya cs <navyacshaji12@gmail.com>
2025-11-12 20:01:37 +0530

    Time: 64 ms (11.9%), Space: 57.9 MB (31.27%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function (nums, n) {
+    let a = []
+    for (let i = 0; i < n; i++) {
+        a.push(nums[i])
+        a.push(nums[i + n])
+    }
+    return a
+};
