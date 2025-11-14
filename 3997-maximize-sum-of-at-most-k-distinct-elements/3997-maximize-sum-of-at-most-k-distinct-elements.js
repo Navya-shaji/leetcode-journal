06486436f7cce07a31eefef552460ea8e2ed67db
Navya cs <navyacshaji12@gmail.com>
2025-11-14 14:44:28 +0530

    Time: 42 ms (5.07%), Space: 64.5 MB (5.07%) - LeetHub

--- 3997-maximize-sum-of-at-most-k-distinct-elements/3997-maximize-sum-of-at-most-k-distinct-elements.js
+++ 3997-maximize-sum-of-at-most-k-distinct-elements/3997-maximize-sum-of-at-most-k-distinct-elements.js
@@ -3,16 +3,16 @@
  * @param {number} k
  * @return {number[]}
  */
-var maxKDistinct = function(nums, k) {
-    let a=[]
-    let count=0
-    let max=nums.sort((a,b)=>b-a)
+var maxKDistinct = function (nums, k) {
+    let a = []
+    let count = 0
+    let max = nums.sort((a, b) => b - a)
     console.log(max)
-    for(let i=0;i<max.length;i++){
-        if(!a.includes(max[i]) && count<k){
+    for (let i = 0; i < max.length; i++) {
+        if (!a.includes(max[i]) && count < k) {
             a.push(max[i])
             count++
         }
     }
-return a    
+    return a
 };
