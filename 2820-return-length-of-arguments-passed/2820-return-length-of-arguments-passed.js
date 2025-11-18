cd4d4a3498e22a6aa3c2fcfcd57d2d9771390165
Navya cs <navyacshaji12@gmail.com>
2025-11-18 18:24:48 +0530

    Time: 50 ms (29.7%), Space: 53.9 MB (69.89%) - LeetHub

--- 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/2820-return-length-of-arguments-passed.js
@@ -2,9 +2,9 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
-var argumentsLength = function(...args) {
-    let count=0
-    for(let i=0;i<args.length;i++){
+var argumentsLength = function (...args) {
+    let count = 0
+    for (let i = 0; i < args.length; i++) {
         count++
     }
 
