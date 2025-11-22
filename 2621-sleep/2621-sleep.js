474233699191f28c612050074a8e056466419d4f
Navya cs <navyacshaji12@gmail.com>
2025-11-22 21:33:18 +0530

    Time: 41 ms (84.43%), Space: 52.8 MB (95.5%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    const result=new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve()
+        },millis)
+    })
+    return result
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
