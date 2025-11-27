3b8cf5bc5d3441fd16946d489e17c3a2691c4022
Navya cs <navyacshaji12@gmail.com>
2025-11-27 21:32:33 +0530

    Time: 0 ms (100%), Space: 55.8 MB (54.61%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -2,22 +2,6 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
-  if(x==0 || x==1) return x
-  let left=0;
-  let right=x;
-  let result=0
-
-  while(left<=right){
-    let mid=Math.floor((left+right)/2)
-    if(mid*mid==x){
-        return mid
-    }else if(mid*mid<x){
-        result=mid;
-        left=mid+1
-    }else{
-        right=mid-1
-    }
-  }
-  return result
+var mySqrt = function (x) {
+    return Math.floor(Math.sqrt(x));
 };
