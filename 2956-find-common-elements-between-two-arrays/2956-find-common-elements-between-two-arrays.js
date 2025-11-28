5ff20f9e60c6e3f8b7dda757c4ee381758bd532b
Navya cs <navyacshaji12@gmail.com>
2025-11-28 21:40:13 +0530

    Time: 6 ms (61.81%), Space: 58.1 MB (72.36%) - LeetHub

---
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var findIntersectionValues = function (nums1, nums2) {
+    let arr = []
+    let count1 = 0
+    let count2 = 0
+    for (let i = 0; i < nums1.length; i++) {
+        if (nums2.includes(nums1[i])) {
+            count1++
+        }
+    }
+    for (let i = 0; i < nums2.length; i++) {
+        if (nums1.includes(nums2[i])) {
+            count2++
+        }
+    }
+    arr.push(count1, count2)
+    return arr
+};
