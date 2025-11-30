317b9ce7d47f9733b62613dba3eb70395c7a6cb8
Navya cs <navyacshaji12@gmail.com>
2025-11-30 21:16:28 +0530

    Time: 5 ms (36.8%), Space: 56.5 MB (50.26%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeOuterParentheses = function (s) {
+    let result = ""
+    let count = 0
+    for (let char of s) {
+        if (char == "(") {
+            if (count > 0) {
+                result += char
+            }
+            count++
+        } else {
+            count--
+            if (count > 0) {
+                result += char
+            }
+        }
+    }
+    return result
+};
