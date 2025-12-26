8c8a279a8c1ffa8f994bdc27dc0cbf2975503a5f
Navya cs <navyacshaji12@gmail.com>
2025-12-26 22:55:52 +0530

    Time: 2 ms (5.81%), Space: 55.4 MB (61.63%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function (nums) {
+    nums.sort((a, b) => a - b);
+    if (nums[0] + nums[1] <= nums[2]) {
+        return "none";
+    } else if (nums[0] === nums[2]) {
+        return "equilateral";
+    } else if (nums[0] === nums[1] || nums[1] === nums[2] || nums[0] === nums[2]) {
+        return "isosceles";
+    } else {
+        return "scalene";
+    }
+};
