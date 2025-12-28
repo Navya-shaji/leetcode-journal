409e042f1d4a373b0bff5fb97614a653a586685e
Navya cs <navyacshaji12@gmail.com>
2025-12-28 15:50:40 +0530

    Time: 4 ms (10.55%), Space: 56.5 MB (33.49%) - LeetHub

---
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var countNegatives = function (grid) {
+    let a = grid.flat()
+    let count = 0
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] < 0) {
+            count++
+        }
+    }
+    return count
+
+};
