c8abdf9992c4010393e9c233e80730cd2b80625c
Navya cs <navyacshaji12@gmail.com>
2026-01-01 16:29:52 +0530

    Time: 0 ms (100%), Space: 53.5 MB (85.71%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -2,7 +2,8 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function(digits) {
+
+var plusOne = function (digits) {
     for (let i = digits.length - 1; i >= 0; i--) {
         if (digits[i] < 9) {
             digits[i]++;
