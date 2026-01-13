e50603242faf091363ad691ca24c3733c1e20290
Navya cs <navyacshaji12@gmail.com>
2026-01-13 10:43:10 +0530

    Time: 167 ms (44.7%), Space: 58.7 MB (35.48%) - LeetHub

---
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} boxes
+ * @return {number[]}
+ */
+var minOperations = function (boxes) {
+    let arr = []
+    for (let i = 0; i < boxes.length; i++) {
+        let ans = 0
+        for (let j = 0; j < boxes.length; j++) {
+            if (boxes[j] == "1") {
+                ans += Math.abs(i - j)
+            }
+        }
+        arr.push(ans)
+    }
+    return arr
+};
