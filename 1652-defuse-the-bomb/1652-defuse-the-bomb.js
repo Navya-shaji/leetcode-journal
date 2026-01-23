9cf1d3a0ebd7b0b171d02e651ddf123ddee421cb
Navya cs <navyacshaji12@gmail.com>
2026-01-23 20:29:54 +0530

    Time: 3 ms (37.07%), Space: 54.7 MB (57.56%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} code
+ * @param {number} k
+ * @return {number[]}
+ */
+var decrypt = function (code, k) {
+    let arr = []
+    for (let i = 0; i < code.length; i++) {
+        let sum = 0
+        if (k > 0) {
+            for (let j = 1; j <= k; j++) {
+                sum += code[(i + j) % code.length]
+            }
+        } else {
+            for (let j = 1; j <= Math.abs(k); j++) {
+                sum += code[(i - j + code.length) % code.length]
+            }
+        }
+        arr.push(sum)
+    }
+    return arr
+};
