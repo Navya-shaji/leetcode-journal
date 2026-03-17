ddf3f3c9abeb539cc77eca6c9e63ec99ce759f78
Navya cs <navyacshaji12@gmail.com>
2026-03-17 18:35:17 +0530

    Time: 20 ms (5.26%), Space: 61.5 MB (5.26%) - LeetHub

---
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var luckyNumbers = function (matrix) {
+    let arr = []
+    for (let i = 0; i < matrix.length; i++) {
+        let min = Math.min(...matrix[i])
+        let index = matrix[i].indexOf(min)
+        for (let j = 0; j < matrix.length; j++) {
+            let res = matrix.map(item => item[index])
+            let max = Math.max(...res)
+            if (max == min) {
+                arr.push(max)
+                break
+            }
+        }
+    }
+    return arr
+};
