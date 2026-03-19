cfb40edb6ae61e501c3b7bbae1484363183ca494
Navya cs <navyacshaji12@gmail.com>
2026-03-19 15:10:26 +0530

    Time: 1 ms (57.81%), Space: 53.6 MB (100%) - LeetHub

---
+++ 2869-minimum-operations-to-collect-elements/2869-minimum-operations-to-collect-elements.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function (nums, k) {
+
+    let count = 0
+    let set = new Set()
+    for (let i = nums.length - 1; i >= 0; i--) {
+        count++
+        if (nums[i] <= k) {
+            set.add(nums[i])
+        }
+        if (set.size === k) {
+            return count
+        }
+    }
+
+};
