367709da6bfb960c303ec0fc16a2e4b5cfd3ffcc
Navya cs <navyacshaji12@gmail.com>
2026-03-31 10:42:57 +0530

    Time: 124 ms (5.69%), Space: 57.7 MB (82.55%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let count = 1
+    for (let i = 1; i <= nums.length; i++) {
+        if (nums[i] == nums[i - 1]) {
+            count++
+        } else {
+            count = 1
+        }
+        if (count > 2) {
+            nums.splice(i, 1)
+            i--
+        }
+    }
+};
