8fa0f634b34249c8177744dbfde484a2985732ac
Navya cs <navyacshaji12@gmail.com>
2026-04-13 12:39:01 +0530

    Time: 1 ms (36.73%), Space: 52.8 MB (100%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.js
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.js
@@ -4,6 +4,12 @@
  * @param {number} start
  * @return {number}
  */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @param {number} start
+ * @return {number}
+ */
 var getMinDistance = function (nums, target, start) {
     let min = Infinity
     for (let i = 0; i < nums.length; i++) {
