5f149ff0b1e12d84ae27f41bca8e2fa3222b32f0
Navya cs <navyacshaji12@gmail.com>
2026-06-08 11:56:38 +0530

    Time: 40 ms (27.27%), Space: 85.5 MB (90.21%) - LeetHub

---
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} pivot
+ * @return {number[]}
+ */
+var pivotArray = function (nums, pivot) {
+    let left = []
+    let right = []
+    let mid = []
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < pivot) {
+            left.push(nums[i])
+        } else if (nums[i] == pivot) {
+            mid.push(nums[i])
+        } else {
+            right.push(nums[i])
+        }
+    }
+    return [...left, ...mid, ...right]
+};
