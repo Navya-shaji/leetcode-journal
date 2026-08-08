6edae3634693161f5dff656f0a7d71495254c9a7
Navya cs <navyacshaji12@gmail.com>
2026-08-08 22:40:22 +0530

    Time: 111 ms (65.63%), Space: 70.1 MB (34.38%) - LeetHub

---
+++ 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var partitionArray = function (nums, k) {
+    nums.sort((a, b) => a - b)
+    let res = 1
+    let min = nums[0]
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] - min > k) {
+            res++
+            min = nums[i]
+        }
+    }
+    return res
+};
